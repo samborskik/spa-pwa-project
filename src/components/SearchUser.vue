2ddf8d1d2257a6171426d01becd3d41e61ef9870
Jakub <jakubrola98@gmail.com>
2020-04-28 19:36:51 +0200

    HomePage/SearchUser fix

--- src/components/SearchUser.vue
+++ src/components/SearchUser.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     
-     <b-input-group class="mt-4-d-inline-flex p-2 bd-highlight w-25 p-3 ml-auto mr-auto">
-    <b-form-input v-model="userName"></b-form-input>
-    <b-input-group-append>
-      <b-button class="raddish" variant="primary" @click="handleSearchClick">Search</b-button>
-    </b-input-group-append>
-  </b-input-group>
+    <b-input-group class="mt-4-d-inline-flex p-2 bd-highlight w-25 p-3 ml-auto mr-auto">
+      <b-form-input v-model="userName"></b-form-input>
+      <b-input-group-append>
+        <b-button class="raddish" variant="primary" @click="handleSearchClick">Search</b-button>
+      </b-input-group-append>
+    </b-input-group>
 
   </div>
 
