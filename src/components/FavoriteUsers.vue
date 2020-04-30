c16146a428c985c02d035c5085b6d4e091ed6a3a
jakubpodolski <jmp.podolski@gmail.com>
2020-04-30 20:39:41 +0200

    Fixed rerender

--- src/components/FavoriteUsers.vue
+++ src/components/FavoriteUsers.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
-    <Loader v-if="isLoading"/>
+    <Loader v-if="isLoading === true"/>
     <div v-else>
-      <p>{{this.users}}</p>
+      <ul>
+        <li v-for="user in users" :key="user.name">
+          <p>{{user.username}}</p>
+          <p>{{user.clan}}</p>
+          <p>{{user.honor}}</p>
+          <button @click="removeUsersFromFavorites(user.username)">
+            Remove
+          </button>
+        </li>
+      </ul>
+      
     </div>
   </div>
 </template>
@@ -15,7 +25,7 @@ export default {
   name: 'FavoriteUsers',
   data: () => ({
     users: [],
-    isLoading: true,
+    isLoading: false,
   }),
   components: {
     Loader
@@ -26,27 +36,37 @@ export default {
       const db = firebase.firestore();
       const appUser = localStorage.getItem('spa-pwa-project');
       const dbRef = db.collection(appUser);
+      this.isLoading = true;
+
       dbRef.get()
         .then(function (querySnapshot) {
+          
           querySnapshot.forEach(function(doc) {
             users.push(doc.data())
-            console.log(doc.data())
           });
         })
         .catch(er => console.log(er))
+
+      this.isLoading = false;
     },
     removeUsersFromFavorites: function(user) {
       const db = firebase.firestore();
       const appUser = localStorage.getItem('spa-pwa-project');
       const dbRef = db.collection(appUser);
+      this.isLoading = true;
+
       dbRef.doc(user).delete()
         .then(alert("User deleted"))
         .catch(er => console.log(er))
+
+      const index = this.users.indexOf(user);
+      this.users.splice(index, 1);
+
+      this.isLoading = false;
     }
   },
   beforeMount: async function() {
     await this.getFavoriteUsers();
-      this.isLoading = false;
   }
 }
 </script>
