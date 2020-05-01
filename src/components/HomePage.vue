ce6c7fcd68064b17c953d7e46de0676dbabe5e62
Krzysztof Samborski <krzysztofsamborski@icloud.com>
2020-05-02 01:11:23 +0200

    Add responsive navbar

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <div>
-    <b-button class="float-sm-right" @click='handleChangeFormClick'>
-      <span v-if="changeView">Show Favorites</span>
-      <span v-else>Back to Search</span>
-    </b-button>
-    </div>
-    <div v-if="changeView">
+    <nav>
+      <b-button class="float-sm-right" @click='handleChangeFormClick'>
+        <span v-if="changeView">Show Favorites</span>
+        <span v-else>Back to Search</span>
+      </b-button>
       <b-button class="float-sm-right" @click="handleUserLogout" variant="primary">Logout!</b-button>
+    </nav>
+    <div v-if="changeView">
       <SearchUser :getUserData="this.getUserData" />
     <Loader v-if="isLoading" />
     <User
@@ -15,7 +15,6 @@
       :userData="this.userData"/>
     </div>
     <div v-else>
-      <b-button class="float-sm-right" @click="handleUserLogout" variant="primary">Logout!</b-button>
       <FavoriteUsers />
     </div>
   </div>
