fbb17d1ecdc05ea78818645bb6bc5d4176eb4f97
Krzysztof Samborski <krzysztofsamborski@icloud.com>
2020-05-02 11:16:54 +0200

    Add way of display favourite users to table

--- src/components/FavoriteUsers.vue
+++ src/components/FavoriteUsers.vue
@@ -1,22 +1,22 @@
 <template>
   <div>
-    <Loader v-if="isLoading === true"/>
-    <div v-else>
-      <ul>
-        <li v-for="user in users" :key="user.name">
-          <p>{{user.username}}</p>
-          <p>{{user.clan}}</p>
-          <p>{{user.honor}}</p>
-          <button @click="removeUsersFromFavorites(user.username)">
-            Remove
-          </button>
-          <button>
-            Edit
-          </button>
-        </li>
-      </ul>
+      <Loader v-if="isLoading === true"/> 
+    <b-container v-else class="shadow-lg m-auto">
+      <b-row v-for="user in users" :key="user.name ">
+        <!-- class="shadow-lg p-3 col-12 col-md-6 col-xl-3" -->
+        <b-col ><b-avatar rounded="lg" size="72px"></b-avatar></b-col>
+          <b-col><p>{{user.username}}</p></b-col>
+          <b-col><p>{{user.clan || 'No clan'}}</p></b-col>
+          <b-col><p>{{user.honor}}</p></b-col>
+          <b-col><b-button variant="primary" @click="removeUsersFromFavorites(user.username)">
+            <i class="fa fa-trash">
+          </b-button></b-col>
+          <b-col><b-button>
+            <i class="fa fa-user-edit"></i>
+          </b-button></b-col>
+      </b-row>
       
-    </div>
+    </b-container>
   </div>
 </template>
 
